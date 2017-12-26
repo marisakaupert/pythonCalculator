7dd41628bd0e9f837ca2dc73b7ad68dd37ef8925
marisaslozano <marisalozano1995@gmail.com>
2017-12-26 15:49:53 -0500

    complete addition function

--- main.py
+++ main.py
@@ -2,4 +2,30 @@ import math
 
 
 class MainFunctions(object):
-    pass
+    def add(self, *args):
+        result = sum(args)
+        return result
+
+    def subtract(self, *args):
+        pass
+
+    def multiply(self):
+        pass
+
+    def divide(self):
+        pass
+
+    def modulus(self):
+        pass
+
+    def squareRoot(self):
+        pass
+
+    def square(self):
+        pass
+
+    def cube(self):
+        pass
+
+    def inverse(self):
+        pass
