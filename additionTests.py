16e44facc7fde5f5e6349658d8440b88229f6923
marisaslozano <marisalozano1995@gmail.com>
2017-12-27 13:42:25 -0500

    seperate addition function tests to own file

---
+++ additionTests.py
@@ -0,0 +1,27 @@
+import unittest
+from main import MainFunctions
+
+
+class AdditionTests(unittest.TestCase):
+    def setUp(self):
+        self.test = MainFunctions()
+
+    def test_adding_two_numbers(self):
+        result = self.test.add(5, 3)
+        self.assertEqual(8, result)
+
+    def test_adding_multiple_numbers(self):
+        result = self.test.add(5, 3, 10)
+        self.assertEqual(18, result)
+
+    def test_adding_negative_numbers(self):
+        result = self.test.add(-10, 5, -10)
+        self.assertEqual(-15, result)
+
+    def test_adding_no_numbers(self):
+        result = self.test.add()
+        self.assertEqual(0, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
