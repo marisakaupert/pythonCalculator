f1c17738a0a3327fd6f98d02ae5e4b30c237e342
marisaslozano <marisalozano1995@gmail.com>
2018-01-01 17:36:19 -0500

    add squaring function unit tests

---
+++ powerOfTwoTests.py
@@ -0,0 +1,27 @@
+import unittest
+from main import MainFunctions
+
+
+class SquaredTests(unittest.TestCase):
+    def setUp(self):
+        self.test = MainFunctions()
+        self.square = 2
+
+    def test_square_a_number(self):
+        result = self.test.powerOfTwo(12)
+        self.assertEqual(144, result)
+
+    def test_square_a_negative_number(self):
+        result = self.test.powerOfTwo(-1)
+        self.assertEqual(1, result)
+
+    def test_square_zero(self):
+        result = self.test.powerOfTwo(0)
+        self.assertEqual(0, result)
+
+    def test_square_invalid_number(self):
+        result = self.test.powerOfTwo(None)
+        self.assertEqual("Invalid Input", result)
+
+if __name__ == '__main__':
+    unittest.main()
