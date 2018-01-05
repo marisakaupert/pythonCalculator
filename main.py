71df7fae756be62a3fcd443a0a987c34ae1866f1
marisaslozano <marisalozano1995@gmail.com>
2018-01-05 13:40:26 -0500

    create main function that loads ui

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+import os
+from design import Ui_Dialog
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class MainWindow(QtWidgets.QDialog, Ui_Dialog):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.makeConnections()
+
+    def makeConnections(self):
+        pass
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
