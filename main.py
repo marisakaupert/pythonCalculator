36aa6f45e6521251b8cbfdc7ca68debb83d60123
marisaslozano <marisalozano1995@gmail.com>
2018-01-06 15:18:06 -0500

    create a stylesheet within my main file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from design import Ui_Dialog
+from arithmaticFunctions import MainFunctions
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
@@ -8,6 +9,23 @@ class MainWindow(QtWidgets.QDialog, Ui_Dialog):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
+        self.setStyleSheet("""
+            QPushButton {
+                border: none;
+                background-color: #8f8f91;
+                font-size: 16px;
+            }
+
+            QLineEdit {
+                font-size: 35 px;
+                margin: 10px;
+            }
+
+            QLabel {
+                font-size: 11px;
+            }
+            
+        """)
         self.makeConnections()
 
     def makeConnections(self):
