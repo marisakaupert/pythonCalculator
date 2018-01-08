6147c959a1045298a49c0a9f1a7869aaca2d1945
marisaslozano <marisalozano1995@gmail.com>
2018-01-08 13:58:32 -0500

    delete py file of stylesheet and open qss file

--- main.py
+++ main.py
@@ -8,42 +8,9 @@ class MainWindow(QtWidgets.QDialog, Ui_Dialog):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
-        self.setStyleSheet("""
-            QDialog {
-                background-color: #b6b6b6;
-            }
-            QPushButton {
-                border: none;
-                background-color: #d3d3d3;
-                font-size: 16px;
-                min-width: 150px;
-                min-height: 80px;
-                font-family: monotype;
-            }
-            QPushButton#zeroPushButton, QPushButton#onePushButton,
-            QPushButton#twoPushButton, QPushButton#threePushButton,
-            QPushButton#fourPushButton, QPushButton#fivePushButton,
-            QPushButton#sixPushButton, QPushButton#sevenPushButton,
-            QPushButton#eightPushButton, QPushButton#ninePushButton {
-                background-color: #f1f1f1;
-                font-weight: bold;
-            }
-            QLineEdit {
-                font-size: 35px;
-                padding: 10px;
-                border: none;
-                background-color: #b6b6b6;
-            }
-            QLabel {
-                font-size: 15px;
-            }
-            QListWidget {
-                min-width: 300px;
-                min-height: 100px;
-                background-color: #b6b6b6;
-                border: none;
-            }
-        """)
+        sshFile = "stylesheet.qss"
+        with open(sshFile, "r") as fh:
+            self.setStyleSheet(fh.read())
         self.makeConnections()
 
     def makeConnections(self):
