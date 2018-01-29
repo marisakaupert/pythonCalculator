788a1bce433be0d3930a9c37935a8cdcc2279602
marisaslozano <marisalozano1995@gmail.com>
2018-01-29 13:25:52 -0800

    fix formatting

--- calculator/ui/design.py
+++ calculator/ui/design.py
@@ -75,7 +75,6 @@ class Ui_Dialog(object):
                     self.mathButtons.setObjectName('digitButtons')
 
                 self.buttonsGroup.addButton(self.mathButtons)
-
                 self.buttonsGridLayout.addWidget(self.mathButtons, i, j)
 
         self.calculationsVerticalLayout.addLayout(self.buttonsGridLayout)
